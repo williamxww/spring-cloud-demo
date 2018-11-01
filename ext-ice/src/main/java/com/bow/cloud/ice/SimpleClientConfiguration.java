60d4d952b1d707be6ed6e0f95c3701dba85f682c
williamxww <vivid_xiang@163.com>
2018-11-01 23:32:31 +0800

    u

--- ext-ice/src/main/java/com/bow/cloud/ice/SimpleClientConfiguration.java
+++ ext-ice/src/main/java/com/bow/cloud/ice/SimpleClientConfiguration.java
@@ -1,22 +1,16 @@
 package com.bow.cloud.ice;
 
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.netflix.ribbon.StaticServerList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.netflix.client.config.DefaultClientConfigImpl;
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.IPing;
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.NoOpPing;
-import com.netflix.loadbalancer.RandomRule;
-import com.netflix.loadbalancer.Server;
-import com.netflix.loadbalancer.ServerList;
-
 /**
- * @see IceClientConfiguration
  * @author vv
+ * @see IceClientConfiguration
  * @since 2018/4/6.
  */
 @Configuration
@@ -44,12 +38,13 @@ public class SimpleClientConfiguration {
         return new NoOpPing();
     }
 
-    @Bean
-    @ConditionalOnMissingBean
-    @SuppressWarnings("unchecked")
-    public ServerList<Server> serverList(IClientConfig config) {
-        Server server  = new Server("ice","127.0.0.1",10000);
-        StaticServerList serverList = new StaticServerList(server);
-        return serverList;
-    }
+	@Bean
+	@ConditionalOnMissingBean
+	@SuppressWarnings("unchecked")
+	public ServerList<Server> serverList(IClientConfig config) {
+		// Server server = new Server("ice","127.0.0.1",10000);
+		Server server = new Server("127.0.0.1", 10000);
+		StaticServerList serverList = new StaticServerList(server);
+		return serverList;
+	}
 }
